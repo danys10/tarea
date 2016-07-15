59a8ca8a1553e3c8f5f3d00bcf07bead52e60de2
danys10 <bochito_amarillo10@hotmail.com>
2016-07-14 22:13:29 -0500

    Add files via upload
    Integrantes de Equipo
    Daniel Alberto Velasco Valerio
    Fernando Arturo Hernandez Torres
    David Abraham Hernandez Olivera
    Cesar Antonio Garcia Tolentino 
    Ivan Josue Mendez Perez

---
+++ proyectoPolanco2.py
@@ -0,0 +1,74 @@
+
+mensaje = "ingrese el tamaño del arreglo"
+
+	print mensaje
+
+t = raw_input()
+:
+
+lista = []
+for i in range(0, int(t)):
+	
+
+lista.append(raw_input("Ingrese los datos -> "))
+
+
+
+print "A^0 = E"
+#
+resultado = "{
+"
+for i in range(0, len(lista)):
+	resultado = resultado + lista[i]+","
+resultado = resultado + "}"
+
+
+print "A^1 = "+a
+#
+resultado2 = "{
+"
+for i in range(0, len(lista)):
+	for j in range(0, len(lista)):	
+		
+
+	resultado2 = resultado2 + lista[i]+lista[j]+","
+resultado2 = resultado2 + "}"
+
+
+print "A^2 = "+b
+#
+resultado3 = "{
+"
+for i in range(0, len(lista)):
+	for j in range(0, len(lista)):	
+
+		for k in range(0, len(lista)):
+			
+
+resultado3 = resultado3 + lista[i]+lista[j]+lista[k]+","
+resultado3 = resultado3 + "}"
+
+
+print "A^3 = "+c
+#
+resultado4 = "{
+"
+for i in range(0, len(lista)):
+	for j in range(0, len(lista)):	
+
+		for k in range(0, len(lista)):
+
+			for l in range(0, len(lista)):
+				
+
+resultado4 = resultado4 + lista[i]+lista[j]+lista[k]+lista[l]+","
+resultado = len(d)
+d[a-2].replace(",","}")
+#resultado4 = resultado4 + "}"
+
+
+print "A^4 = "+resultado4
+#print "------------------------------"
+#print len(cadena)
+#for i in range(0, 10,2):
+
